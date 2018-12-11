f871751ed6e5154f5a8cc9bda555f826f617ce1f
beausimpson <simpson.beau@gmail.com>
2018-12-11 00:32:59 -0600

    customer functionality works, needs to be refactored. added console.table npm for table display

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,6 +5,7 @@ var inquirer = require("inquirer");
 // mySql module
 var mysql = require("mysql");
 
+require("console.table");
 
 
 // creates the connection to the sql database
@@ -21,37 +22,35 @@ var connection = mysql.createConnection({
     password: "13bs13bs!!!",
     database: "bamazon"
 });
+
 // connect to the mysql server and sql database
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("\n------------- You are now connected to the database! -------------\n------------- Welcome to Bamazon -------------\n");
+    console.log("\n------------- Welcome to Bamazon -------------\n");
     // connection.end();
 });
 
 function startBamazon() {
-    // connectDatabase();
-    // connect to the database function
 
     // after the connection is made
     // all products are selected from database and displayed on screen
-    connection.query("SELECT * FROM products", function (err, productResponse) {
+    connection.query("SELECT item_id, product_name, price FROM products", function (err, productResponse) {
         if (err) throw err;
 
-        var products = [];
-        for (var i = 0; i < productResponse.length; i++) {
-            products.push("ID#: " + productResponse[i].item_id +
-                " Name: " + productResponse[i].product_name +
-                " Price: $" + productResponse[i].price);
-        };
+        console.table(productResponse);
 
         inquirer
             .prompt([
                 {
-                    type: "list",
+                    type: "input",
                     message: "Choose by ID, which product you would like to buy:",
-                    choices: products,
-                    name: "selectedProduct",
-                    pageSize: productResponse.length
+                    name: "productId",
+                    validate: function (value) {
+                        if (isNaN(value) === false) {
+                            return true;
+                        }
+                        return false;
+                    }
                 },
                 {
                     type: "input",
@@ -66,12 +65,50 @@ function startBamazon() {
                 }
             ])
             .then(function (itemResponse) {
-                console.log(itemResponse.selectedProduct)
-                stopConnection();
+                var products = [];
+                for (var i = 0; i < productResponse.length; i++) {
+                    products.push(productResponse[i].item_id);
+                };
+
+                var idNum = parseInt(itemResponse.productId);
+                if (products.includes(idNum)) {
+                    connection.query("SELECT stock_quantity FROM products WHERE ?",
+                        { item_id: idNum }, function (err, idResponse) {
+                            if (err) throw err;
+
+                            var buyUnits = parseInt(itemResponse.unitsToBuy);
+                            var stockQuantity = idResponse[0].stock_quantity;
+                            if (buyUnits > stockQuantity) {
+                                console.log("Insufficient quantity!")
+                                stopConnection();
+                            } else {
+                                var updatedQuantity = stockQuantity - buyUnits;
+
+                                connection.query("UPDATE products SET ? WHERE ?", [
+                                    {
+                                        stock_quantity: updatedQuantity
+                                    },
+                                    {
+                                        item_id: idNum
+                                    }
+                                ],
+                                    function (err, quantityUpdate) {
+                                        connection.query("SELECT price FROM products WHERE ?", { item_id: idNum }, function (err, itemPrice) {
+                                            console.log("You owe $" + itemPrice[0].price * buyUnits)
+                                            stopConnection();
+                                        })
+                                    }
+                                )
+
+                            }
+                        });
+                } else {
+                    console.log("Sorry, that is not an actual product id")
+                    stopConnection();
+                }
             });
-
-
     });
+
 };
 
 function stopConnection() {
